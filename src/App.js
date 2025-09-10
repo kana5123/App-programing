69583447087b1092b7f57fc4ddb6d3767d120c5e
Kang Ei Yui <kangeiyui512@gmail.com>
2025-09-10 10:37:36 +0900

    앱프 제제

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Title, { Counter } from "./components/앱프 1차 과제";
 function App() {
   return (
     <div style={{ padding: 24 }}>
-      <Title name="강이의" dep="제주대학교" 인공지능학과 number={2021104002} />
+      <Title name="강이의" dep="제주대학교 인공지능학과" number={2021104002} />
       <Counter />
     </div>
   );
